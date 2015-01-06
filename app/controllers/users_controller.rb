9715b81801945ad5be5b8a9559c3225527b2e828
Thomas Noe <thomasjnoe@gmail.com>
2015-01-06 00:00:42 -0500

    Finish create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 
   def show
