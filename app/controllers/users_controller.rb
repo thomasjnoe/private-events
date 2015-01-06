738782f1d31bfc00ab42192f12698f669ae75916
Thomas Noe <thomasjnoe@gmail.com>
2015-01-06 00:44:58 -0500

    Login user after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		flash[:success] = "Thank you for signing up!  Welcome to Private Events!"
+  		log_in @user
   		redirect_to @user
   	else
   		render 'new'
