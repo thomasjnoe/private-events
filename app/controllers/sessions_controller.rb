24527cabf53861506cdbcf20d2957bcbc5470a9e
Thomas Noe <thomasjnoe@gmail.com>
2015-01-06 00:16:41 -0500

    Add session create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	@user = User.find_by(email: params[:session][:email].downcase)
+  	if @user
+  		log_in @user
+  		redirect_to @user
+  	else
+  		flash.now[:danger] = "User does not exist"
+  		render 'new'
+  	end
   end
 
   def destroy
