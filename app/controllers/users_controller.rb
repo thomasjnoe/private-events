8aefdf227788ed311a52a9cdcaf79ade6bd4f5c6
Thomas Noe <thomasjnoe@gmail.com>
2015-01-06 00:02:01 -0500

    Add user strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,10 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name, :email)
+  	end
 end
