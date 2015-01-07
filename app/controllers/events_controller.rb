74a0e681194f52e479d742b0c0725c7a02e8b65c
Thomas Noe <thomasjnoe@gmail.com>
2015-01-07 01:42:18 -0500

    Finish new, create and show methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,26 @@
 class EventsController < ApplicationController
   def new
+  	@event = Event.new
   end
 
   def create
+  	@event = current_user.created_events.build(event_params)
+  	if @event.save
+  		flash[:success] = "Event successfully created!"
+  		redirect_to @event
+  	else
+  		render 'new'
+  	end
   end
 
   def show
+  	@event = Event.find(params[:id])
+  	@user = User.find_by(id: @event.creator_id)
   end
+
+  private
+
+  	def event_params
+  		params.require(:event).permit(:title, :description, :location, :date)
+  	end
 end
