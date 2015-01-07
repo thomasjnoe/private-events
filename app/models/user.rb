d989aa2056204ad9f9c27f38126bd5522ef3ec4d
Thomas Noe <thomasjnoe@gmail.com>
2015-01-07 01:10:55 -0500

    Add user and event association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
+	has_many :created_events, :foreign_key => :creator_id, :class_name => "Event"
 end
