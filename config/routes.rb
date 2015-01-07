09c4beb7d1db77156d60fbe1a76e3b88b4db653f
Thomas Noe <thomasjnoe@gmail.com>
2015-01-07 01:30:48 -0500

    Add events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'events/new'
+
+  get 'events/create'
+
+  get 'events/show'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
