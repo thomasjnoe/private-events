df3ba4c2abae25cdeb4e9b23d2ae64989a3de70e
Thomas Noe <thomasjnoe@gmail.com>
2015-01-07 23:12:57 -0500

    Add event attendances associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
+	has_many :event_attendances, :foreign_key => :attended_event_id
+	has_many :attendees, :through => :event_attendances
 	belongs_to :creator, :class_name => "User"
 end
