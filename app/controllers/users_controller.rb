cf5d416e151d8487d0abf390a9402e313009fedc
Thomas Noe <thomasjnoe@gmail.com>
2015-01-08 00:25:18 -0500

    remove unneeded created and attended events instance variables from show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,6 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  	@created_events = @user.created_events
-  	@attended_events = @user.attended_events
   end
 
   private
