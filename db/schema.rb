217f239eed0f5ab30c80fdb898d1bc9c4a7a8b1d
Thomas Noe <thomasjnoe@gmail.com>
2015-01-07 23:07:41 -0500

    Add event_attendances model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107060622) do
+ActiveRecord::Schema.define(version: 20150108040658) do
+
+  create_table "event_attendances", force: true do |t|
+    t.integer  "attended_event_id"
+    t.integer  "attendee_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "events", force: true do |t|
     t.string   "title"
