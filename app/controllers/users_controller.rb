1e5c570349dd5967cdbc9781530636036405e058
Thomas Noe <thomasjnoe@gmail.com>
2015-01-08 00:08:09 -0500

    Add created and attended events instance variables to show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@created_events = @user.created_events
+  	@attended_events = @user.attended_events
   end
 
   private
