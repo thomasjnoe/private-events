1c4215af9251eba67b21bab43636d179eb6a514b
Thomas Noe <thomasjnoe@gmail.com>
2015-01-08 00:24:29 -0500

    Add Upcoming and Previously Created and Attended event methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ActiveRecord::Base
 	has_many :created_events, :foreign_key => :creator_id, :class_name => "Event"
 	has_many :event_attendances, :foreign_key => :attendee_id
 	has_many :attended_events, :through => :event_attendances
+	
+	def upcoming_events_attending
+		self.attended_events.upcoming
+	end
+
+	def previously_attended_events
+		self.attended_events.past
+	end
+
+	def upcoming_created_events
+		self.created_events.upcoming
+	end
+
+	def past_created_events
+		self.created_events.past
+	end
 end
