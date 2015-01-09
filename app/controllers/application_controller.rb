1c83549e5517050cd84a15ec4e290e3ee5753cc4
Thomas Noe <thomasjnoe@gmail.com>
2015-01-09 00:39:54 -0500

    Add logged_in_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def logged_in_user
+  	unless logged_in?
+  		store_forwarding_url
+  		flash[:danger] = "Please log in first."
+  		redirect_to login_path
+  	end
+  end
 end
